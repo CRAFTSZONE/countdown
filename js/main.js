f3ed953c796841c29e597bf3fc84bea6c5ab34d4
Joshua Minja <61590875+JoshuaMinja@users.noreply.github.com>
2020-12-12 09:15:14 +0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -41,9 +41,7 @@
         	{ src: "images/slides/02.jpg" },
         	{ src: "images/slides/01.jpg" },
         	{ src: "images/slides/05.jpg" },
-        	{ src: "images/slides/07.jpg" },
-		{ src: "images/slides/09.jpg" },
-		{ src: "images/slides/10.jpg" }
+        	{ src: "images/slides/07.jpg" }
     	]
 	});
 
